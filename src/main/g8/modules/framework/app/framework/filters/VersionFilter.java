7b7f0d1265ce63287099a4059f78167d8b2a6eff
张强 <qiang.zhang@touchair.cn>
2018-07-03 11:23:22 +0800

    修复 不恰当的描述和实现。

--- src/main/g8/modules/framework/app/framework/filters/VersionFilter.java
+++ src/main/g8/modules/framework/app/framework/filters/VersionFilter.java
@@ -14,13 +14,13 @@ import play.mvc.Result;
 @Singleton
 public final class VersionFilter extends Filter {
 
-  private final Config conf;
+  private final String apiVersion;
   private final Executor exec;
 
   @Inject
   public VersionFilter(Materializer mat, Config conf, Executor exec) {
     super(mat);
-    this.conf = conf;
+    this.apiVersion = conf.getString("app.version");
     this.exec = exec;
   }
 
@@ -28,8 +28,6 @@ public final class VersionFilter extends Filter {
   public CompletionStage<Result> apply(
       Function<RequestHeader, CompletionStage<Result>> next,
       RequestHeader req) {
-    // 不作为类变量是因为，需要能够在运行时修改并立即生效
-    String apiVersion = conf.getString("app.version");
     return next.apply(req).thenApplyAsync(
         result -> result.withHeader("X-Version", apiVersion),
         exec);
